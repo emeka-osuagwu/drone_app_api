1b12f704362a0c53581e80282309f5db38a89714
Emeka Osuagwu <emekaosuagwu@hotmail.com>
2016-08-01 12:52:33 +0100

    update

---
+++ database/migrations/2016_07_19_142150_create_video_migration.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVideoMigration extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('videos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->jsonb('urls')->nullable();
+            $table->integer('user_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('videos');
+    }
+}
