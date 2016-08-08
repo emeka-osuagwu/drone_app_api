88d7b3b5678e66b0d200489e4fc099328ba0e1c9
Emeka Osuagwu <emekaosuagwu@hotmail.com>
2016-08-08 12:20:27 +0100

    remove header

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,7 +13,7 @@
 
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE');
-header( 'Access-Control-Allow-Headers: Authorization, Content-Type' );
+
 
 Route::get('/', function (){
 	return view('index');
