3a9cd05b969f86128e02c200ecdec4b5542af027
Emeka Osuagwu <emekaosuagwu@hotmail.com>
2016-08-09 09:05:01 +0100

    update app

--- app/Http/Middleware/CORS.php
+++ app/Http/Middleware/CORS.php
@@ -17,7 +17,6 @@ class CORS
     {
         return $next($request)
             ->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE')
-            ->header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, X-Requested-With, token');
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     }
 }
